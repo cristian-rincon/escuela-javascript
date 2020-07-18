35cb890e2ee0cc32b1d47f5820f47fac0f67ea0e
cristian-rincon <cristian.o.rincon.b@gmail.com>
2020-07-18 18:48:43 -0500

    se incluye el proyecto del curso de react

---
+++ react/README.md
@@ -0,0 +1,13 @@
+# js-react-1
+Curso de react escuela de javascript
+
+
+## Instalación de dependencias
+
+Para realizar la instalación de las dependencias se usa el siguiente comando:
+
+        npm install
+
+Json-server se debe instalar de forma global con: 
+
+        npm install -g json-server
---
+++ react/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../assets/styles/components/Footer.scss";
+
+const Footer = () => (
+  <footer className="footer">
+    <a href="/">Terminos de uso</a>
+    <a href="/">Declaración de privacidad</a>
+    <a href="/">Centro de ayuda</a>
+    <a href="https://github.com/cristian-rincon">Cristian Orlando Rincón</a>
+  </footer>
+);
+
+export default Footer;
