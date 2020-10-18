d259af354c8e5480ad5660e4353b8cac82ab8083
cristian-rincon <cristian.o.rincon.b@gmail.com>
2020-10-17 21:22:56 -0500

    validator handler & error handler base

--- backend-node/movies-api/index.js
+++ backend-node/movies-api/index.js
@@ -1,16 +1,24 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-const { config } = require('./config/index')
-const moviesApi = require('./routes/movies.js')
+const { config } = require('./config/index');
+const moviesApi = require('./routes/movies.js');
+
+const {
+    logErrors,
+    errorHandler,
+} = require('./utils/middleware/errorHandlers.js');
 
 // body parser
-app.use(express.json())
+app.use(express.json());
 
-moviesApi(app)
+moviesApi(app);
 
-app.listen(config.port, function() {
+app.use(logErrors)
+app.use(errorHandler)
+
+
+app.listen(config.port, function () {
     // eslint-disable-next-line no-console
     console.log(`Listening http://localhost:${config.port}`);
-})
-
+});
