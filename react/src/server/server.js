76c684702b50bbd5cb71d8945e4d36f9ddafd220
cristian-rincon <cristian.o.rincon.b@gmail.com>
2020-11-27 20:42:33 -0500

    ssr-express - modulo 4 - aplicando server side rendering

--- react/src/server/server.js
+++ react/src/server/server.js
@@ -1,15 +1,25 @@
+/* eslint-disable global-require */
 import express from "express";
 import dotenv from "dotenv";
 import webpack from "webpack";
+import React from "react";
+import { renderToString } from "react-dom/server";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { renderRoutes } from "react-router-config";
+import { StaticRouter } from "react-router-dom";
+import serverRoutes from "../frontend/routes/serverRoutes";
+import reducer from "../frontend/reducers";
+import initialState from "../frontend/initialState";
+import Layout from "../frontend/components/Layout";
 
 dotenv.config();
 
 const { ENV, PORT } = process.env;
-
 const app = express();
 
 if (ENV === "development") {
-  console.log(">>> Development Config");
+  console.log("Development config");
   const webpackConfig = require("../../webpack.config");
   const webpackDevMiddleware = require("webpack-dev-middleware");
   const webpackHotMiddleware = require("webpack-hot-middleware");
@@ -17,37 +27,48 @@ if (ENV === "development") {
   const serverConfig = { serverSideRender: true };
 
   app.use(webpackDevMiddleware(compiler, serverConfig));
-  app.use(webpackHotMiddleware(compiler)); // Hot mode replacement en todo el proyecto
+  app.use(webpackHotMiddleware(compiler));
 }
 
-app.get("*", (req, res) => {
-  res.send(
-    `<!DOCTYPE html>
-    <html lang="en">
-    
+const setResponse = (html, preloadedState) => {
+  return `
+  <!DOCTYPE html>
+  <html>
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="assets/app.css">
-        <title>Fleetingvideo</title>
+      <link rel="stylesheet" href="assets/app.css" type="text/css">
+      <title>Platzi Video</title>
     </head>
-    
     <body>
-        <div id="app">
-    
-        </div>
-        <script src="assets/app.js" type="text/javascript"> </script>
+      <div id="app">${html}</div>
+      <script>
+      window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(
+        /</g,
+        "\\u003c"
+      )}
+    </script>
+      <script src="assets/app.js" type="text/javascript"></script>
     </body>
-    
-    </html>`
+  </html>
+  `;
+};
+
+const renderApp = (req, res) => {
+  const store = createStore(reducer, initialState);
+  const preloadedState = store.getState();
+  const html = renderToString(
+    <Provider store={store}>
+      <StaticRouter location={req.url} context={{}}>
+        <Layout>{renderRoutes(serverRoutes)}</Layout>
+      </StaticRouter>
+    </Provider>
   );
-});
+
+  res.send(setResponse(html, preloadedState));
+};
+
+app.get("*", renderApp);
 
 app.listen(PORT, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(`Server running on port ${PORT}`);
-  }
+  if (err) console.log(err);
+  else console.log("Server running on port 3000");
 });
