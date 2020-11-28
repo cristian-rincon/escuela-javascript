9109ddf618820146e791c3ac105c9732a7679f3e
cristian-rincon <cristian.o.rincon.b@gmail.com>
2020-11-27 21:38:24 -0500

    Optimización del Build

--- react/src/server/server.js
+++ react/src/server/server.js
@@ -2,6 +2,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import webpack from "webpack";
+import helmet from "helmet";
+
 import React from "react";
 import { renderToString } from "react-dom/server";
 import { Provider } from "react-redux";
@@ -28,6 +30,11 @@ if (ENV === "development") {
 
   app.use(webpackDevMiddleware(compiler, serverConfig));
   app.use(webpackHotMiddleware(compiler));
+} else {
+  app.use(express.static(`${__dirname}/public`));
+  app.use(helmet());
+  app.use(helmet.permittedCrossDomainPolicies());
+  app.disable("x-powered-by");
 }
 
 const setResponse = (html, preloadedState) => {
@@ -62,6 +69,8 @@ const renderApp = (req, res) => {
       </StaticRouter>
     </Provider>
   );
+
+  res.set("Content-Security-Policy", "img-src 'self' http://dummyimage.com");
 
   res.send(setResponse(html, preloadedState));
 };
--- react/webpack.config.js
+++ react/webpack.config.js
@@ -1,23 +1,35 @@
-import dotenv from "dotenv";
 const path = require("path");
 const webpack = require("webpack");
 // const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const TerserPlugin = require("terser-webpack-plugin");
 
-dotenv.config();
+require("dotenv").config();
 
-const { ENV } = process.env;
+const isDev = process.env.ENV === "development";
+const entry = ["./src/frontend/index.js"];
+
+if (isDev) {
+  entry.push(
+    "webpack-hot-middleware/client?path=/__webpack_hmr&timeout=2000&reload=true"
+  );
+}
 
 module.exports = {
-  mode: ENV,
-  entry: ["./src/frontend/index.js", "webpack-hot-middleware/client?path=/__webpack_hmr&timeout=2000&reload=true"],
+  entry,
+  mode: process.env.ENV,
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "src/server/public"),
     filename: "assets/app.js",
     publicPath: "/",
   },
   resolve: {
     extensions: [".js", ".jsx"],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
   },
   module: {
     rules: [
@@ -25,10 +37,6 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: "babel-loader",
-      },
-      {
-        test: /\.html$/,
-        use: "html-loader",
       },
       {
         test: /\.(s*)css$/,
@@ -57,11 +65,11 @@ module.exports = {
     historyApiFallback: true,
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    /* new HtmlWebpackPlugin({
-      template: "public/index.html",
-      filename: "index.html",
-    }), */
+    isDev ? new webpack.HotModuleReplacementPlugin() : () => {},
+    isDev ? () => {} : new CompressionWebpackPlugin({
+      test: /\.js$|\.css$/,
+      filename: '[path][base].gz',
+    }),
     new MiniCssExtractPlugin({
       filename: "assets/app.css",
     }),
